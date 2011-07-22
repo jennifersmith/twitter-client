adbfde094348074c0c58420c98022ae4ccec5a9e
Jennifer Smith <jennifer@home>
2011-07-22 16:32:30 +0100

    Can get some basic stuff back from flickr

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
 goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.string.StringBuffer', 'goog.object', 'goog.array']);
-goog.addDependency("../AkDcw.js", ['main'], ['cljs.core']);
+goog.addDependency("../EUFPx.js", ['main'], ['cljs.core', 'goog.net.Jsonp', 'goog.events', 'goog.dom']);
--- src/main.clj
+++ src/main.clj
@@ -1,9 +1,26 @@
-(ns main)
-(def jquery (js* "$"))
+(ns main 
+  (:require 
+    [goog.net.Jsonp :as jsonp]
+    [goog.events :as events]
+    [goog.dom :as dom]))
 
-(.ready (jquery (js* "document"))
-   (fn []
-     (-> (jquery "div.meat")
-         (.html "This is a test.")
-         (.append "<div>Look here!</div>"))))
+(def flickr-uri "http://api.flickr.com/services/feeds/photos_public.gne")
+
+(defn log [& args]
+  (js* "console.log(~{})" (apply pr-str args)))
+
+(defn receive-kittens [json]
+  (let [{:keys [items]} (js->clj json :keywordize-keys true)]
+    (log (count items))))
+
+(defn get-kittens []
+  (.send (goog.net.Jsonp. flickr-uri "jsoncallback")
+        (.strobj
+          {"tags" "cat" "tagmode" "any" "format" "json"})
+        receive-kittens))
+
+(events/listen (dom/getElement "go") events/EventType.CLICK
+  get-kittens)
+
+
 
